c462b0b52c4f4c3c4ac5388426b11e8dc7b636d0
huhangbo <1746982699@qq.com>
2023-05-18 19:34:36 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Col, Row, Statistic, Progress} from 'antd';
+import { red, green, orange } from '@ant-design/colors';
+import {useState} from "react";
+import ShowGauge from "./gauge";
 
-function App() {
+
+const normalColor = '#3f8600'
+const abnormalColor = '#cf1322'
+
+export default function App() {
+  const [danger, setDanger] = useState(1) // 危险性
+
+  const [oxygen, setOxygen] = useState(15) // 氧气
+
+  const [co2, setCo2] = useState(0) // 二氧化碳
+
+  const [co1, setCo1] = useState(0) // 一氧化碳
+
+  const [degree, setDegree] = useState(0) // 温度
+
+  const [ch4, setCh4] = useState(0) // 甲烷
+
+  const [smoke, setSmoke] = useState(0) // 烟雾
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{maxWidth: 800, width: '80%', margin: '50px auto 0 auto', backgroundColor: 'rgb(33,40,48)', textAlign: 'center'}}>
+      <Row style={{color: 'white'}}>
+        <Col span={12} push={6} style={{margin: '20px 0 12px 0'}}>
+          <h1 className="title" style={{margin: '0 auto'}}>注氮智能化控制系统</h1>
+        </Col>
+        <Col span={3} push={8} style={{margin: '25px 0 0 0'}}>
+          <div className="danger" >
+            自燃危险性
+            <Progress style={{marginTop: 5}} size={20} showInfo={false} percent={danger/3 * 100} steps={3} strokeColor={[green[6], orange[6], red[6]]} />
+          </div>
+        </Col>
+      </Row>
+      <div style={{width: '70%', margin: '0 auto'}}>
+
+        <Row style={ {color: 'white', margin: '40px 0 0 0'}} justify={'space-between'}>
+
+          <Col span={5} >
+              <div>
+                氧气浓度
+              </div>
+            <Statistic value={oxygen}  valueStyle={{color: oxygen < 20 ? normalColor: abnormalColor}} precision={2} suffix="%"/>
+          </Col>
+
+          <Col span={5}>
+            <div>
+              二氧化碳浓度
+            </div>
+            <Statistic  value={co2} valueStyle={{color: co2 > 10 ? normalColor: abnormalColor}} suffix="ppm" />
+          </Col>
+
+          <Col span={5}>
+            <div>
+              温度
+            </div>
+            <Statistic value={degree} value={co2} valueStyle={{color: degree > 10 ? normalColor: abnormalColor}} suffix="°C" />
+          </Col>
+
+        </Row>
+
+        <Row style={ {color: 'white', margin: '40px 0 0 0'}} justify={'space-between'}>
+          <Col span={5} >
+            <div>
+              一氧化碳浓度
+            </div>
+            <Statistic  value={co1} value={co2} valueStyle={{color:co1 > 10 ? normalColor: abnormalColor}} suffix="ppm" />
+          </Col>
+
+          <Col span={5}>
+            <div>
+              甲烷浓度
+            </div>
+            <Statistic  value={ch4} value={co2} valueStyle={{color: ch4 > 10 ? normalColor: abnormalColor}} suffix="ppm" />
+          </Col>
+
+          <Col span={5}>
+            <div>
+              烟雾报警器状态
+            </div>
+            <Statistic value={smoke} valueStyle={{color: smoke <= 100 ? normalColor: abnormalColor}} />
+          </Col>
+
+        </Row>
+
+      </div>
+
+
+      <Row align="middle">
+        <Col span={4} offset={3}>
+          <div style={{margin: '25px 0'}}>
+            <Button type={'default'} ghost={true} onClick={() => {
+              // 点击事件
+            }}>
+              手动注氮按钮
+            </Button>
+          </div>
+
+          <div style={{margin: '25px 0'}}>
+            <Button danger={true} style={{backgroundColor: "transparent"}} onClick={() => {
+              // 点击事件
+            }}>
+              自动布距注氮
+            </Button>
+          </div>
+
+        </Col>
+
+        <Col span={6} offset={6} style={{marginTop: -60}}>
+          <ShowGauge/>
+        </Col>
+      </Row>
+
     </div>
   );
 }
-
-export default App;
---
+++ src/gauge.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect} from 'react';
+import { Gauge } from '@ant-design/plots';
+
+const ShowGauge = () => {
+    const ticks = [0, 1 / 3, 2 / 3, 1]
+    const color = ['#F4664A', '#FAAD14', '#30BF78']
+    const [data, setData] = useState(0)
+    useEffect(() => {
+        let data = 0.1;
+        const interval = setInterval(() => {
+            data += 0.1
+            setData(data - Math.trunc(data))
+        }, 500);
+    }, [])
+    const config = {
+        percent: data,
+        range: {
+            ticks: [0, 1],
+            color: ['l(0) 0:#F4664A 0.5:#FAAD14 1:#30BF78'],
+        },
+        axis: {
+            label: {
+                formatter(v) {
+                    return Number(v) * 1500;
+                },
+            },
+            subTickLine: {
+                count: 3,
+            },
+        },
+        indicator: {
+            pointer: {
+                style: {
+                    stroke: '#D0D0D0',
+                },
+            },
+            pin: {
+                style: {
+                    stroke: '#D0D0D0',
+                },
+            },
+        },
+        statistic: {
+            title: {
+                formatter: ({ percent }) => {
+                    return (Number(percent)*1500).toFixed(0);
+                },
+                style: ({ percent }) => {
+                    return {
+                        fontSize: '24px',
+                        lineHeight: 1,
+                        color: percent < ticks[1] ? color[0] : percent < ticks[2] ? color[1] : color[2],
+                    };
+                },
+            },
+            content: {
+                offsetY: 36,
+                style: {
+                    fontSize: '24px',
+                    color: '#4B535E',
+                },
+                formatter: () => '动态注氮流量',
+            },
+        }
+    }
+    return <Gauge {...config} />;
+}
+export default ShowGauge
